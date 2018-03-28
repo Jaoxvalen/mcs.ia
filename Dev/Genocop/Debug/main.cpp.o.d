cbef852f00be4327b09109af4c28d31145ec096f
Joao <jaoxvalen@gmail.com>
2018-03-27 20:52:58 -0500

    genocop added

---
+++ Dev/Genocop/Debug/main.cpp.o.d
@@ -0,0 +1,3 @@
+Debug/main.cpp.o: main.cpp Genocop.h
+
+Genocop.h:
---
+++ Dev/Genocop/main.cpp
@@ -0,0 +1,16 @@
+#include "Genocop.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+
+Genocop *genocop;
+int main(int argc, char** argv)
+{
+    genocop = new Genocop("../t01");
+    return 0;
+}
